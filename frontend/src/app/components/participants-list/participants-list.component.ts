b95836c6dfe679c9620d572db02c97c21656c9ff
Raduna Daniel <danielraduna12@gmail.com>
2023-07-06 17:55:23 +0300

    feat: participants list frontend start

--- frontend/src/app/components/participants-list/participants-list.component.ts
+++ frontend/src/app/components/participants-list/participants-list.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {User} from "../../entities/user";
+import {ActivatedRoute} from "@angular/router";
+import {EventService} from "../../services/event.service";
+import {Event} from "../../entities/event";
 
 @Component({
   selector: 'app-participants-list',
   templateUrl: './participants-list.component.html',
   styleUrls: ['./participants-list.component.scss']
 })
-export class ParticipantsListComponent {
+export class ParticipantsListComponent implements OnInit {
+  event!: Event;
+  users: User[] = [];
 
+  constructor(
+    private route: ActivatedRoute,
+    private eventService: EventService
+  ) {
+  }
+
+  ngOnInit() {
+    this.getEvent();
+  }
+
+  getEvent(): void {
+    const id = +this.route.snapshot.paramMap.get('id')!;
+    this.eventService.getEventById(id).subscribe(data => {
+      this.event = data.body!;
+      this.users = this.event.users;
+    });
+  }
 }
